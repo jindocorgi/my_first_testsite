d8a046ddb3aa47691340ac6102f6e7ba9d151263
Jindocorgi <110286688+jindocorgi@users.noreply.github.com>
2022-09-18 14:56:27 +0900

    3rdcommint
    css다듬

---
+++ script.js
@@ -0,0 +1,25 @@
+//캔버스세팅
+let canvas;
+let ctx;
+canvas = document.createElement("canvas")
+ctx = canvas.getContext("2d")
+canvas.width=400;
+canvas.heigt=200;
+document.body.appendChild(canvas);
+
+
+tID = 0;
+const h1 = document.getElementById("time")
+const start = document.getElementById("start")
+let applicant =['starbeany','iam_shen2','ddoong_ee.co','bori_kaebi','moongchi_yam','apolon_thelagotto','minvelyss','bori_201030','sa_pphi_','noel_is_angel_of_july','elizabeth_victoria_dd','s2.doopal_beagle.s2','qqwa_12_','cong_190905','harujeong___','sunset_0820','with_a._.smile','mochi__210723','lovemarie83','ponyoriya','kim_bolhwi','fury_serry','jihye_9103','santa811_sister','jjang_0321','corgi_bt','butter____world','mozzi_ccomi_ddubi','suna_sia','adam_220627']
+
+function choose(){
+    let rand = Math.floor(Math.random() * applicant.length);
+    h1.innerHTML = applicant[rand]
+}
+
+start.onclick = function(){
+    tID = setInterval(choose,30)
+    setTimeout(()=>{clearInterval(tID);},1500);
+}
+
