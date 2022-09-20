095dcdce4f00c517bfb4c6ecb609ac03e87cb8c2
Jindocorgi <110286688+jindocorgi@users.noreply.github.com>
2022-09-20 11:35:10 +0900

    7th_commint
    어레이수정

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 tID = 0;
 const h1 = document.getElementById("time")
 const start = document.getElementById("start")
-let applicant =['starbeany','iam_shen2','ddoong_ee.co','bori_kaebi','moongchi_yam','apolon_thelagotto','minvelyss','bori_201030','sa_pphi_','noel_is_angel_of_july','elizabeth_victoria_dd','s2.doopal_beagle.s2','qqwa_12_','cong_190905','harujeong___','sunset_0820','with_a._.smile','mochi__210723','lovemarie83','ponyoriya','kim_bolhwi','fury_serry','jihye_9103','santa811_sister','jjang_0321','corgi_bt','butter____world','mozzi_ccomi_ddubi','suna_sia','adam_220627','bichon_muffin1001','charze_s2','bbobbo._.corgi','bboyboy21','_with_cream','munji_i_i','jjosim_ba','tofu.choi','golden_yusik','moongchi_yam','lovemarie83','kim_bolhwi','corgi_bt','butter____world','moongchi_yam','lovemarie83','kim_bolhwi','corgi_bt','butter____world']
+let applicant =['starbeany','iam_shen2','ddoong_ee.co','bori_kaebi','moongchi_yam','apolon_thelagotto','minvelyss','bori_201030','noel_is_angel_of_july','s2.doopal_beagle.s2','qqwa_12_','cong_190905','harujeong___','sunset_0820','with_a._.smile','mochi__210723','lovemarie83','ponyoriya','kim_bolhwi','fury_serry','jihye_9103','jjang_0321','corgi_bt','butter____world','mozzi_ccomi_ddubi','suna_sia','adam_220627','bichon_muffin1001','charze_s2','bbobbo._.corgi','bboyboy21','munji_i_i','jjosim_ba','golden_yusik','ddoong_ee.co','apolon_thelagotto','sunset_0820','moongchi_yam','minvelyss','bori_201030','lovemarie83','kim_bolhwi','butter____world','starbeany','moongchi_yam','bori_201030','s2.doopal_beagle.s2','lovemarie83','ponyoriya','kim_bolhwi','jihye_9103','jjang_0321','charze_s2','bbobbo._.corgi','jjosim_ba','golden_yusik']
 let result;
 let rand;
 let box1 = document.getElementById("box1");
